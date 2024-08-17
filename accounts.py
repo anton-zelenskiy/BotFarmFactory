8bfa2711b009c807b9de745c8df9e1ea4034a502
Anton Zelenskiy <antonfewwt@gmail.com>
2024-08-18 00:55:54 +0700

    fix

--- accounts.py
+++ accounts.py
@@ -1,8 +1,17 @@
-TELEGRAM_ACCOUNTS = [
-    dict(phone='+995579323517'),
-]
+import os
+import logging
 
-try:
-    from accounts_local import *
-except ImportError:
-    pass
+logger = logging.getLogger(__name__)
+
+
+def init_telegram_accounts() -> list[dict]:
+    phones = [
+        dict(phone=phone.strip())
+        for phone in os.getenv('TELEGRAM_ACCOUNTS').split(',')
+    ]
+
+    logger.info(f'got telegram accounts: {phones}')
+    return phones
+
+
+TELEGRAM_ACCOUNTS = init_telegram_accounts()
